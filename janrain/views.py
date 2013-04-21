401f1b09ff4bb6f64d969401b64d776eff0d7aa4
ckarmann <github@nnamrak.org>
2013-04-20 23:06:26 -0400

    Simple hacks to use Ajax instead of redirecting.

--- janrain/views.py
+++ janrain/views.py
@@ -9,28 +9,41 @@ from janrain import api
 from janrain.models import JanrainUser
 from janrain.signals import *
 
+from django.utils import simplejson
+
+
+def returnError(message) :
+    data = { "status" : "e", "error" : message}
+    return HttpResponse(simplejson.dumps(data), content_type="application/json")
+
+
 @csrf_exempt
 def login(request):
     pre_login.send(JanrainSignal, request=request)
+
+    # get Janrain token    
     try:
         token = request.POST['token']
     except KeyError:
         # TODO: set ERROR to something
         login_failure.send(JanrainSignal, message='Error retreiving token', data=None)
-        return HttpResponseRedirect('/')
+        return returnError('Error retreiving token')
 
+    # retrieve profile
     try:
         profile = api.auth_info(token)
     except api.JanrainAuthenticationError:
         login_failure.send(JanrainSignal, message='Error retreiving profile', data=None)
-        return HttpResponseRedirect('/')
+        return returnError('Error retreiving profile')
     post_profile_data.send(JanrainSignal, profile_data=profile)
 
+    # authenticate
     u = None
     p = profile['profile']
     u = auth.authenticate(profile=p)
     post_authenticate.send(JanrainSignal, user=u, profile_data=profile)
 
+    # get or create Janrain user in DB
     juser = JanrainUser.objects.get_or_create(
                 user=u,
                 username=p.get('preferredUsername'),
@@ -42,26 +55,27 @@ def login(request):
     juser.save()
     post_janrain_user.send(JanrainSignal, janrain_user=juser, profile_data=profile)
 
+    # login in django
     if u is not None:
         request.user = u
         auth.login(request, u)
         post_login.send(JanrainSignal, user=u, profile_data=profile)
 
-    try:
-        redirect = pre_redirect.send(JanrainSignal, type='login', 
-                redirect=request.GET.get('next', '/'))[-1][1]
-    except IndexError:
-        redirect = '/'
-    return HttpResponseRedirect(redirect)
+    # answer
+    data = { "status" : "m", "message" : "good." }
+    return HttpResponse(simplejson.dumps(data), content_type="application/json")
 
 def logout(request):
     pre_logout.send(JanrainSignal, request=request)
+
+    # logout from django
     auth.logout(request)
-    try:
-        redirect = pre_redirect.send(JanrainSignal, type='logout', 
-                redirect=request.GET.get('next', '/'))[-1][1]
-    except IndexError:
-        redirect = '/'
+    # try:
+    #     redirect = pre_redirect.send(JanrainSignal, type='logout', 
+    #             redirect=request.GET.get('next', request.META["HTTP_REFERER"]))[-1][1]
+    # except IndexError:
+    #     redirect = '/'
+    redirect = request.META["HTTP_REFERER"]
     return HttpResponseRedirect(redirect)
 
 def loginpage(request):
